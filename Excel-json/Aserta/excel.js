c811baf3769900c52cac639b8bfbfb9ae5f83d90
valeria <valeria.carrillofig@gmail.com>
2022-10-02 22:51:02 -0500

    Aserta

--- Excel-json/Aserta/excel.js
+++ Excel-json/Aserta/excel.js
@@ -26,7 +26,8 @@ document.getElementById('button').addEventListener("click", () => {
          workbook1.SheetNames.forEach(sheet => {
             //console.log(workbook1.Sheets[sheet]);                           // EL RANGO ES LO GRANDE DEL ENCABEZADO
                 objetoAserta = XLSX.utils.sheet_to_row_object_array(workbook1.Sheets[sheet], {range:1}); //Nombre del array
-             console.log(objetoAserta);
+             console.log("ASERTA")
+                console.log(objetoAserta);
          });
 
          if(selectedFile2){ //Función que convierte SICAS en array de objetos
@@ -39,40 +40,39 @@ document.getElementById('button').addEventListener("click", () => {
                   objetoSICAS = XLSX.utils.sheet_to_row_object_array(workbook2.Sheets[sheet]); //Nombre del array
 
                     //La tabla tiene atributo HIDDEN para que no se vea, pero ahí está.
-                  let tabla ="<table id='BerkleyFianzas' width='80%' border='1' cellpadding='0' cellspacing='0' bordercolor='#0000001' hidden> <tr><th>Póliza</th><th>Prima Neta</th><th>% Comisión</th><th>Total Comisión</th><th>Diferencia comisión</th></tr>";
+                  let tabla ="<table id='Aserta' width='80%' border='1' cellpadding='0' cellspacing='0' bordercolor='#0000001'> <tr><th>Póliza</th><th>Prima Neta</th><th>% Comisión</th><th>Total Comisión</th><th>Diferencia comisión</th></tr>";
                   let resultObject;
                   let sicas;
-                  var encontrar;                 
+                  var encontrar;           
 
                   //Encontrar un valor ahí adentro
-                  search = (key, ArrayBerkley) => {
-                      /*for (let i=0; i < ArrayBerkley.length; i++) {
-                          if (ArrayBerkley[i].FIANZA == key) {
-                            if (ArrayBerkley[i].INCLUSION == inclu) {
+                  search = (key, ArreyAserta) => {
+                    
+                      for (let i=0; i < ArreyAserta.length; i++) {
+                        var polizaAserta = String(ArreyAserta[i]["No Fianza/\nCertificado"])
+                          if (polizaAserta == key) {
                             encontrar=1;
-                            //compara las primas netas y si son diferentes las mete en la tabla.
-                            if(ArrayBerkley[i]["PRIMA NETA"] != sicas["PrimaNeta"]){
-                                var diferencia= Math.round((ArrayBerkley[i]["PRIMA NETA"] -sicas["PrimaNeta"])*100)/100;
+                            if(ArreyAserta[i]["Prima Neta"] != sicas["PrimaNeta"]){
+                                var diferencia= Math.round((ArreyAserta[i]["Prima Neta"] -sicas["PrimaNeta"])*100)/100;
                                 console.log("La diferencia es de"+diferencia);
-                                tabla=tabla+"<tr><td style='background-color:var(--bs-azul3)'>"+ArrayBerkley[i].FIANZA+"-"+ArrayBerkley[i].INCLUSION+"</td><td>"+sicas["PrimaNeta"]+"</td><td>"+sicas["% Participacion"]+"</td><td>"+sicas["Importe"]+"</td><td style='color:var(--bs-rojo1)'>"+diferencia+"</td></tr>";
-                            }
-                              return ArrayBerkley[i];
+                                tabla=tabla+"<tr><td style='background-color:var(--bs-azul3)'>"+ArreyAserta[i]["No Fianza/"]+"</td><td>"+sicas["PrimaNeta"]+"</td><td>"+sicas["% Participacion"]+"</td><td>"+sicas["Importe"]+"</td><td style='color:var(--bs-rojo1)'>"+diferencia+"</td></tr>";
                             }
                           }
+
                       }
                       if(encontrar==0){
-                      tabla= tabla+"<tr><td style='background-color:var(--bs-azul3)'>"+key+"-"+inclu+"</td><td>"+sicas["PrimaNeta"]+"</td><td>"+sicas["% Participacion"]+"</td><td>"+sicas["Importe"]+"</td><td style='background-color:var(--bs-rojo2)'>NO SE ENCONTRÓ</td></tr>";
-                      }
-                      encontrar=0;*/ 
+                        tabla= tabla+"<tr><td style='background-color:var(--bs-azul3)'>"+key+"</td><td>"+sicas["PrimaNeta"]+"</td><td>"+sicas["% Participacion"]+"</td><td>"+sicas["Importe"]+"</td><td style='background-color:var(--bs-rojo2)'>NO SE ENCONTRÓ</td></tr>";
+                        }
+                        encontrar=0;
                     }
                     for(var j=0; j<objetoSICAS.length; j++){ //Ciclo que va a buscar cada poliza de SICAS en Berkley
-                        var poliza = Number(objetoSICAS[j].Poliza)
+                        var poliza =  String (objetoSICAS[j].Poliza)
+                        sicas=objetoSICAS[j];
                       resultObject = search(poliza, objetoAserta)
-                      console.log(resultObject);
                       console.log("Número de registros en sicas: "+j);
                      document.getElementById("jsondata").innerHTML = tabla+"</table>"; //Se manda la tabla pero no se va a ver porque tiene HIDDEN
                     }
-                    //ExportToExcel('xlsx'); //Se llama la función para que convierta a XLSX directamente.
+                    ExportToExcel('xlsx'); //Se llama la función para que convierta a XLSX directamente.
                     if(resultObject==0){
                         document.getElementById("jsondata").innerHTML = "No se encontró ninguna fianza";
 
@@ -93,9 +93,9 @@ document.getElementById('button').addEventListener("click", () => {
 });
 
 function ExportToExcel(type, fn, dl) {// función que convierte a excel
-    var elt = document.getElementById('BerkleyFianzas');
+    var elt = document.getElementById('Aserta');
     var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
     return dl ?
       XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }):
-      XLSX.writeFile(wb, fn || ('Berkley_Fianzas_Comparacion_Mayo.' + (type || 'xlsx')));
+      XLSX.writeFile(wb, fn || ('Aseguradora Aserta.' + (type || 'xlsx')));
  }
